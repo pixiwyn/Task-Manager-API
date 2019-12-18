2d87d19efd2003fbedace86e6a95f90926030092
pixiwyn <pixiwyn@gmail.com>
2019-12-18 01:01:46 -0700

    tweak cors rules

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers);
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
