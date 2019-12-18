d7b5cd5d2bbf617809d5ba7a3ccf023abd37703a
pixiwyn <pixiwyn@gmail.com>
2019-12-18 01:12:29 -0700

    tweak cors rules

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
@@ -6,12 +7,7 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();
-});
+app.use(cors());
 
 // Setup JSON Returns
 app.use(express.json());
